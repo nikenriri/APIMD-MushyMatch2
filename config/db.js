dbcf4d543bae1f7cccd9708e588de941d36931ad
niken riri <72871424+nikenriri@users.noreply.github.com>
2023-06-02 22:28:23 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const db = mysql.createPool({
     host: "34.101.59.135",
     user: "mushymatch",
     password: "mushymatch",
-    database: "mushymatch_db"
+    database: "database_mushymatch"
 });
 
 // conn.getConnection((err) => {
